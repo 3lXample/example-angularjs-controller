79a822b50c90653d5918f322df0f49cc58cfbdd2
Anisetus Elly Efendi <3lly3fendi@gmail.com>
2017-08-18 23:47:46 +0700

    Add AngularJS Skeleton

---
+++ src/_app/controller.js
@@ -0,0 +1,14 @@
+/**
+ * example-angularjs-controller
+ * https://github.com/3lXample/example-angularjs-controller
+ *
+ * Copyright (c) 2017 3lXample (https://github.com/3lXample)
+ * Licensed under the MIT license
+ */
+(function() {
+  'use strict';
+
+  var name   = '3XApp';
+  var module = angular.module(name);
+
+})();
