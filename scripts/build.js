58de634d02e7deacb7132905946d4a989e28faf7
Anisetus Elly Efendi <3lly3fendi@gmail.com>
2017-08-18 21:55:18 +0700

    Add script build

---
+++ scripts/build.js
@@ -0,0 +1,26 @@
+/**
+ * example-angularjs-controller
+ * https://github.com/3lXample/example-angularjs-controller
+ *
+ * Copyright (c) 2017 3lXample (https://github.com/3lXample)
+ * Licensed under the MIT license
+ */
+const fs = require('fs-extra');
+
+const srcVendorDir = 'src/_vendor';
+const vendorFile   = [
+  { 'file': 'angular.min.js',        'path': 'node_modules/angular/angular.min.js' },
+  { 'file': 'angular.min.js.map',    'path': 'node_modules/angular/angular.min.js.map' },
+  { 'file': 'bootstrap.min.css',     'path': 'node_modules/bootstrap/dist/css/bootstrap.min.css' },
+  { 'file': 'bootstrap.min.css.map', 'path': 'node_modules/bootstrap/dist/css/bootstrap.min.css.map' },
+  { 'file': 'bootstrap.min.js',      'path': 'node_modules/bootstrap/dist/js/bootstrap.min.js' },
+  { 'file': 'jquery.slim.min.js',    'path': 'node_modules/jquery/dist/jquery.slim.min.js' },
+  { 'file': 'jquery.slim.min.map',   'path': 'node_modules/jquery/dist/jquery.slim.min.map' },
+  { 'file': 'popper.min.js',         'path': 'node_modules/popper.js/dist/umd/popper.min.js' },
+  { 'file': 'popper.min.js.map',     'path': 'node_modules/popper.js/dist/umd/popper.min.js.map' }
+];
+
+// copy vendor file into src vendor directory
+vendorFile.forEach((vendor) => {
+  fs.copySync(vendor.path, srcVendorDir + '/' + vendor.file, { 'overwrite': true, 'errorOnExist': true });
+});
